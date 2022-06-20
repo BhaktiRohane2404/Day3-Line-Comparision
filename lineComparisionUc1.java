be7b2d35656c5e2e946f20f2c56eba3df99889bb
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-06-20 20:14:38 +0530

    Add files via upload

---
+++ lineComparisionUc1.java
@@ -0,0 +1,8 @@
+//To Displaying Welcome To Line Comparision Computation
+package com.lineComparision;
+
+public class lineComparisionUc1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the line comparison computation program.");
+    }
+}
